609eeb2463546a62df2844a5f6acb12588a4d536
ivchicano <46921761+ivchicano@users.noreply.github.com>
2021-04-13 20:42:11 +0200

    Remove remaining Scala files and remake gitignore for python

--- build.sbt
+++
@@ -1,6 +0,0 @@
-libraryDependencies += "com.bot4s" %% "telegram-core" % "4.4.0-RC2"
-libraryDependencies += "io.spray" %%  "spray-json" % "1.3.6"
-libraryDependencies ++= Seq(
-    "net.debasishg" %% "redisclient" % "3.30"
-)
-enablePlugins(JavaAppPackaging)
--- src/main/scala/SpanishPhrasesEnum.scala
+++
@@ -1,7 +0,0 @@
-object SpanishPhrasesEnum {
-  val tormenta = "Hay tormenta en Madrid"
-  val lloviendo = "Está lloviendo en Madrid"
-  val cieloClaro = "Se está DPM en Madrid"
-  val nublado = "Está nublao en Madrid"
-  val nevando = "Está nevando en Madrid"
-}
