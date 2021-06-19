3a96948e9035464e5c8e7b39fbcd91200a74908d
ivchicano <46921761+ivchicano@users.noreply.github.com>
2021-06-20 00:20:58 +0200

    fix restricted_admin

--- madriletabot.py
+++ madriletabot.py
@@ -25,7 +25,7 @@ def restricted_admin(func):
         user_id = update.effective_user.id
         user_status = context.bot.get_chat_member(chat_id=chat_id, user_id=user_id)
         self.logger.info("Soliciting permission. User:" + str(user_id) + " | User status: " + user_status)
-        if (user_id != self.CREATOR) or (user_status != CHATMEMBER_CREATOR) or (
+        if (user_id != self.CREATOR) and (user_status != CHATMEMBER_CREATOR) and (
                 user_status != CHATMEMBER_ADMINISTRATOR):
             update.message.reply_text("No tienes permisos para ejecutar este comando")
             return
