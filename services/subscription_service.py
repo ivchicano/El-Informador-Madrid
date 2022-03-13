567d3c85edda19ff596aa3ab365ed3c94190aa3d
ivchicano <ivchicano@gmail.com>
2022-03-13 09:32:14 +0100

    fix ranking value being sometimes string

--- services/subscription_service.py
+++ services/subscription_service.py
@@ -26,7 +26,7 @@ class SubscriptionService:
         keys = self._r_conn.keys("slots:*:" + str(user_id))
         key = "slots:" + str(user_name) + ":" + str(user_id)
         if (len(keys) > 0) and (keys[0] != key):  # user name changed and has to be updated
-            value = self._r_conn.get(keys[0])
+            value = int(self._r_conn.get(keys[0]))
             self._r_conn.delete(keys[0])
             return self._r_conn.set(key, value + points)
         return self._r_conn.incrby(key, points)
